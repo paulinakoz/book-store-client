547b8be9e587e68d09ed360b237f4e6abe011a21
Paulina <paulinakozaczek@gmail.com>
2022-11-11 10:03:23 +0000

    build fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import baseUrl from "./config";
+import baseUrl from "./config.js";
 
 function App() {
   axios(`${baseUrl}/api/v1/books`).then((books) => {
