14d00b8e8f7f4f41f22a8d5307721e58d5d8a732
Paulina <paulinakozaczek@gmail.com>
2022-11-11 10:51:24 +0000

    pass with no tests fix

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import App from "./App";
-
-describe("App component", () => {
-  it("should render without error", () => {
-    // const { asFragment } = render(<App />);
-    // expect(asFragment()).toMatchSnapshot();
-  });
-});
